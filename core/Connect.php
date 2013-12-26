8513911dab366671f27c933ce9645c7cb3ebf6cd
vietnt134 <vietnt134@gmail.com>
2013-12-26 21:03:55 +0700

    semilast

--- core/Connect.php
+++ core/Connect.php
@@ -16,4 +16,6 @@
 		echo "Failed to connect to MySQL: " . mysql_error();
 	}
 	mysql_select_db($dbname, $my_connect);
-	mysql_query("SET character_set_results=utf8", $my_connect);
+	mysql_query("SET character_set_client=utf8", $my_connect);
+mysql_query("SET character_set_connection=utf8", $my_connect);
+mysql_query("SET character_set_results=utf8", $my_connect);
